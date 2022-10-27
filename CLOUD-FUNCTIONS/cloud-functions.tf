e073debbf26fd14c2d7a43be0ef3ab87b4befb98
ladriano96 <leonardoadriano93@gmail.com>
2022-10-27 15:59:31 -0300

    adicionando o código do cloud function

--- CLOUD-FUNCTIONS/cloud-functions.tf
+++ CLOUD-FUNCTIONS/cloud-functions.tf
@@ -1,3 +1,12 @@
 resource "google_cloudfunctions_function" "ladriano_teste_function" {
-  # (resource arguments)
+  name                          = var.function_name
+  description                   = "descricao da sua funcao aqui"
+  project                       = var.project_id
+  region                        = var.region_name
+  trigger_http                  = true
+  runtime                       = var.runtime_type_python39
+  entry_point                   = var.entry_point_name
+  available_memory_mb           = "2048"
+  max_instances                 = "5"
+  timeout                       = "60"
 }
--- CLOUD-FUNCTIONS/main.tf
+++ CLOUD-FUNCTIONS/main.tf
@@ -10,6 +10,6 @@ terraform {
 provider "google" {
   credentials = file("pj-ladriano-cloud-ecf3b47ae249.json")
 
-  project = var.projeto_name
+  project = var.project_id
   region  = var.region_name
 }
