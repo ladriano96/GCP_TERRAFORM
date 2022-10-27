358b989a5552a4292d5956ed834d1219e8e11dcd
ladriano96 <leonardoadriano93@gmail.com>
2022-10-27 17:10:09 -0300

    removendo bucket

--- CLOUD-FUNCTIONS/bucket.tf
+++ CLOUD-FUNCTIONS/bucket.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "buck-ladriano-dev-01" {
+/*resource "google_storage_bucket" "buck-ladriano-dev-01" {
   project       = var.project_id
   name          = var.bucket_name
   location      = var.region_name
@@ -16,4 +16,4 @@ resource "google_storage_bucket_object" "functionladriano" {
   source     = "functionladriano.py"
   depends_on = [google_storage_bucket.buck-ladriano-dev-01]
 
-}
+}*/
