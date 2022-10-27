f223def75146a03f71dcae1efb5d02e742ba9a01
ladriano96 <leonardoadriano93@gmail.com>
2022-10-27 16:32:23 -0300

    adicionando funcao phyton

--- CLOUD-FUNCTIONS/bucket.tf
+++ CLOUD-FUNCTIONS/bucket.tf
@@ -4,4 +4,14 @@ resource "google_storage_bucket" "buck-ladriano-dev-01" {
   location      = var.region_name
   storage_class = var.storage_class_standard
   force_destroy = false
+  labels = {
+    "enviroment" = "dev"
+  }
+}
+
+resource "google_storage_bucket_object" "archieve" {
+    name = "functionladriano.py"
+    bucket = var.bucket_name
+    source = "/functionladriano.py"
+
 }
