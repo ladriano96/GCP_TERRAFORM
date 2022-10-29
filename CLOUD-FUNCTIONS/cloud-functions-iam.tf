b857321b1e9c9ae32a59d201fb3113f55b9d468e
ladriano96 <leonardoadriano93@gmail.com>
2022-10-29 08:51:59 -0300

    deletando tfstate

--- CLOUD-FUNCTIONS/cloud-functions-iam.tf
+++ CLOUD-FUNCTIONS/cloud-functions-iam.tf
@@ -10,7 +10,7 @@ data "google_iam_policy" "policy-viewer-function" {
 resource "google_cloudfunctions_function_iam_policy" "policy-function" {
   project        = var.project_id
   region         = var.region_name
-  cloud_function = google_cloudfunctions_function.ladriano_teste_function
-  policy_data    = data.google_iam_policy.policy-viewer-function
+  cloud_function = google_cloudfunctions_function.ladriano_teste_function.id
+  policy_data    = data.google_iam_policy.policy-viewer-function.id
   depends_on     = [data.google_iam_policy.policy-viewer-function]
 }
