9ecf2374f646b6a0924e64c224660db5dbea5351
ladriano96 <leonardoadriano93@gmail.com>
2022-10-29 14:44:59 -0300

    .

--- CLOUD-FUNCTIONS/cloud-functions-iam.tf
+++ CLOUD-FUNCTIONS/cloud-functions-iam.tf
@@ -3,7 +3,7 @@ data "google_iam_policy" "data-iam-plcy-function" {
   /* NO CAMPO MEMBERS PODEM SER ADICIONADOS AS SEGUINTES VARIÁVEIS ABAIXO*/
 
   /* 1 - var.membres_user (identificador que representa qualquer pessoa que esteja na internet, com ou sem uma conta google)
-     2 - var.members_allAuthenticatedUsers (identificador que representa qualquer pessoas autenticada com uma conta google ou conta de serviço)
+     2 - var.members_allAuthenticatedUsers (identificador que representa qualquer pessoa autenticada com uma conta google ou conta de serviço)
      3 - var.members_user (endereço de e-mail que representa uma conta específica do google e tenha usuário criado no iam do projeto gcp)
      4 - var.members_serviceAccount (endereço de e-mail que representa uma conta de serviço e tenha usuário criado no iam do projeto gcp)
      5 - var.members_group (um endereço que representa um grupo de e-mail do google)
--- CLOUD-FUNCTIONS/cloud-functions.tf
+++ CLOUD-FUNCTIONS/cloud-functions.tf
@@ -7,7 +7,7 @@ resource "google_cloudfunctions_function" "cloudfunctions_gn1" {
   region                = var.region_name
   trigger_http          = true
   entry_point           = var.entry_point_name
-  source_archive_bucket = google_storage_bucket.buck-ladriano-dev-01.name
+  source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.functionladriano.name
   available_memory_mb   = "2048"
   max_instances         = "5"
