98ef9199d0c4e4df473f05d1875bb70c3a3c7972
ladriano96 <leonardoadriano93@gmail.com>
2022-10-29 11:33:58 -0300

    adicionando variaveis

--- CLOUD-FUNCTIONS/cloud-functions-iam.tf
+++ CLOUD-FUNCTIONS/cloud-functions-iam.tf
@@ -1,31 +1,31 @@
 data "google_iam_policy" "data-iam-plcy-function" {
-    
-                           /* NO CAMPO MEMBERS PODEM SER ADICIONADOS OS SEGUINTES VALORES ABAIXOS*/
 
-/*  1 - allUsers (identificador que representa qualquer pessoa que esteja na internet, com ou sem uma conta google)
-    2 - allAuthenticatedUsers (identificador que representa qualquer pessoas autenticada com uma conta google ou conta de serviço)
-    3 - user:user@gmail.com (endereço de e-mail que representa uma conta específica do google e tenha usuário criado no iam do projeto gcp)
-    4 - serviceAccount:seuserviceaccount@appspot.gserviceaccount.com (endereço de e-mail que representa uma conta de serviço e tenha usuário criado no iam do projeto gcp)
-    5 - group:seugrupo@gmail.com (um endereço que representa um grupo de e-mail do google)
-    6 - domain:domain.com (cominio que representa todos os usuários desse dominio)     */
+  /* NO CAMPO MEMBERS PODEM SER ADICIONADOS OS SEGUINTES VALORES ABAIXOS*/
+
+  /* 1 - var.membres_user (identificador que representa qualquer pessoa que esteja na internet, com ou sem uma conta google)
+     2 - var.members_allAuthenticatedUsers (identificador que representa qualquer pessoas autenticada com uma conta google ou conta de serviço)
+     3 - var.members_user (endereço de e-mail que representa uma conta específica do google e tenha usuário criado no iam do projeto gcp)
+     4 - var.members_serviceAccount (endereço de e-mail que representa uma conta de serviço e tenha usuário criado no iam do projeto gcp)
+     5 - var.members_group (um endereço que representa um grupo de e-mail do google)
+     6 - var.members_domain (cominio que representa todos os usuários desse dominio)     */
 
   binding {
-    role    = "roles/cloudfunctions.admin"
-    members = ["user:leonardoadriano93@gmail.com"]
+    role    = var.cloud_functions_admin
+    members = var.members_user
   }
   binding {
-    role    = "roles/cloudfunctions.invoker"
-    members = ["user:leonardoadriano93@gmail.com"]
-  }
-
-  binding {
-    role    = "roles/cloudfunctions.developer"
-    members = ["user:leonardoadriano93@gmail.com"]
+    role    = var.cloud_functions_invoker
+    members = var.members_user
   }
 
   binding {
-    role    = "roles/cloudfunctions.viewer"
-    members = ["allAuthenticatedUsers"]
+    role    = var.cloud_functions_developer
+    members = var.members_user
+  }
+
+  binding {
+    role    = var.cloud_functions_viewer
+    members = var.members_allAuthenticatedUsers
   }
 
   depends_on = [google_cloudfunctions_function.ladriano_teste_function]
--- CLOUD-FUNCTIONS/variables.tf
+++ CLOUD-FUNCTIONS/variables.tf
@@ -3,37 +3,41 @@ variable "project_id" {
   default = "pj-ladriano-cloud"
 }
 
+
+
 variable "region_name" {
   type    = string
   default = "southamerica-east1"
 }
+
+
 
 variable "function_name" {
   type    = string
   default = "ladriano_teste_function"
 }
 
+
+
 variable "runtime_type" {
-
-  /* você pode definir o campo default dessa variavel de acordo com a linguagem de programação que será executada na sua função. 
-  As linguagens aceitas pelo Cloud Functions está contida nessa documentação https://cloud.google.com/functions/docs/concepts/execution-environment#runtimes */
-
   type    = string
   default = "python39"
-
 }
+
+
 
 variable "entry_point_name" {
   type    = string
   default = "hello_world"
-
 }
+
+
 
 variable "bucket_name" {
   type    = string
   default = "buck-ladriano-dev-01"
-
 }
+
 
 
 variable "storage_class_standard" {
@@ -42,8 +46,85 @@ variable "storage_class_standard" {
 }
 
 
+
 variable "description" {
   type    = string
   default = "the description here"
-
 }
+
+
+
+variable "cloud_functions_admin" {
+  type    = string
+  default = "roles/cloudfunctions.admin"
+}
+
+
+
+variable "cloud_functions_invoker" {
+  type    = string
+  default = "roles/cloudfunctions.invoker"
+}
+
+
+
+variable "cloud_functions_developer" {
+  type    = string
+  default = "roles/cloudfunctions.developer"
+}
+
+
+
+variable "cloud_functions_viewer" {
+  type    = string
+  default = "roles/cloudfunctions.viewer"
+}
+
+
+
+variable "members_allUsers" {
+  type    = list(any)
+  default = ["allUsers"]
+}
+
+
+
+variable "members_user" {
+  type    = list(any)
+  default = ["user:leonardoadriano93@gmail.com"]
+}
+
+
+
+variable "members_allAuthenticatedUsers" {
+  type    = list(any)
+  default = ["allAuthenticatedUsers"]
+}
+
+
+
+variable "members_serviceAccount" {
+  type    = list(any)
+  default = ["serviceAccount:seuserviceaccount@appspot.gserviceaccount.com"]
+}
+
+
+
+variable "members_group" {
+  type    = list(any)
+  default = ["group:seugrupo@gmail.com"]
+}
+
+
+
+variable "members_domain" {
+  type    = list(any)
+  default = ["domain:seudomain.com"]
+}
+
+
+
+
+
+
+
