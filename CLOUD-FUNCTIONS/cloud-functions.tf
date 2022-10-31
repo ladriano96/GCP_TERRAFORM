f68eb2a2148a9136bc15b71c1c2a61c7023ab420
ladriano96 <leonardoadriano93@gmail.com>
2022-10-31 12:56:47 -0300

    .

--- CLOUD-FUNCTIONS/cloud-functions.tf
+++ CLOUD-FUNCTIONS/cloud-functions.tf
@@ -14,7 +14,7 @@ resource "google_cloudfunctions_function" "cloudfunctions_g1_01" {
   source_archive_bucket = google_storage_bucket.storage_bucket.name
   source_archive_object = google_storage_bucket_object.storage_bucket_object.name
   /* (Se voce quiser usar vpc connector na function, basta descomentar a linha abaixo e criar o connector do arquivo vpc-connector.tf) */
-  vpc_connector         = var.vpc_connector 
+  vpc_connector = var.vpc_connector
 
   labels = {
     "enviroment" = "dev"
@@ -39,12 +39,12 @@ resource "google_cloudfunctions_function" "cloudfunctions_g1_02" {
   source_archive_bucket = google_storage_bucket.storage_bucket.name
   source_archive_object = google_storage_bucket_object.storage_bucket_object.name
   vpc_connector         = var.vpc_connector
-  available_memory_mb   = "2048"
-  max_instances         = "5"
+  available_memory_mb   = "512"
+  max_instances         = "3"
   timeout               = "60"
   event_trigger {
-    event_type = "google.pubsub.topic.publish"
-    resource   = "projects/pj-ladriano-cloud/topics/topic-functions"
+    event_type = var.pub_sub
+    resource   = var.pub_sub_topic
     failure_policy {
       retry = true
     }
--- CLOUD-FUNCTIONS/variables.tf
+++ CLOUD-FUNCTIONS/variables.tf
@@ -1,83 +1,83 @@
 variable "project_id" {
   description = "the name project id"
-  type    = string
-  default = "pj-ladriano-cloud"
+  type        = string
+  default     = "pj-ladriano-cloud"
 }
 
 variable "env" {
   description = "the environment name"
-  type    = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
 
 
 variable "region_name" {
   description = "the name region"
-  type    = string
-  default = "southamerica-east1"
+  type        = string
+  default     = "southamerica-east1"
 }
 
 
 variable "vpc" {
   description = "the vpc name"
-  type = string
-  default = "vpc-ladriano-01"
-  
+  type        = string
+  default     = "vpc-ladriano-01"
+
 }
 
 variable "vpc_connector" {
   description = "the vpc connector name"
-  type = string
-  default = "projects/pj-ladriano-cloud/locations/southamerica-east1/connectors/cnt-dev-01"
-  
+  type        = string
+  default     = "projects/pj-ladriano-cloud/locations/southamerica-east1/connectors/cnt-dev-01"
+
 }
 
 
 variable "subnet" {
   description = "the subnet name"
-  type = string
-  default = "10.152.0.0/28"
-  
+  type        = string
+  default     = "10.152.0.0/28"
+
 }
 
 
 variable "function_name" {
   description = "the cloud function name"
-  type    = string
-  default = "cloudfunctions_gn1"
+  type        = string
+  default     = "cloudfunctions_gn1"
 }
 
 
 
 variable "runtime_type" {
   description = "a programming language to use"
-  type    = string
-  default = "python39"
+  type        = string
+  default     = "python39"
 }
 
 
 
 variable "entry_point_name" {
   description = "the name entry point"
-  type    = string
-  default = "hello_world"
+  type        = string
+  default     = "hello_world"
 }
 
 
 
 variable "bucket_name" {
   description = "the bucket name"
-  type    = string
-  default = "buck-dev-01"
+  type        = string
+  default     = "buck-dev-01"
 }
 
 
 
 variable "storage_class_standard" {
   description = "the type class storage"
-  type    = string
-  default = "STANDARD"
+  type        = string
+  default     = "STANDARD"
 }
 
 
@@ -91,85 +91,97 @@ variable "description" {
 
 variable "cloud_functions_admin" {
   description = "the cloud functions admin"
-  type    = string
-  default = "roles/cloudfunctions.admin"
+  type        = string
+  default     = "roles/cloudfunctions.admin"
 }
 
 
 
 variable "cloud_functions_invoker" {
   description = "the cloud functions invoker"
-  type    = string
-  default = "roles/cloudfunctions.invoker"
+  type        = string
+  default     = "roles/cloudfunctions.invoker"
 }
 
 
 
 variable "cloud_functions_developer" {
   description = "the cloud functions developer"
-  type    = string
-  default = "roles/cloudfunctions.developer"
+  type        = string
+  default     = "roles/cloudfunctions.developer"
 }
 
 
 
 variable "cloud_functions_viewer" {
   description = "the cloud functions viewer"
-  type    = string
-  default = "roles/cloudfunctions.viewer"
+  type        = string
+  default     = "roles/cloudfunctions.viewer"
 }
 
 
 
 variable "members_allUsers" {
   description = "allowing all users with internet access"
-  type    = list(any)
-  default = ["allUsers"]
+  type        = list(any)
+  default     = ["allUsers"]
 }
 
 
 
 variable "members_user" {
   description = "allowing email with google account"
-  type    = list(any)
-  default = ["user:leonardoadriano93@gmail.com"]
+  type        = list(any)
+  default     = ["user:leonardoadriano93@gmail.com"]
 }
 
 
 
 variable "members_allAuthenticatedUsers" {
   description = "allowing authenticated users in gcp project"
-  type    = list(any)
-  default = ["allAuthenticatedUsers"]
+  type        = list(any)
+  default     = ["allAuthenticatedUsers"]
 }
 
 
 
 variable "members_serviceAccount" {
   description = "allowing service account in gcp project"
-  type    = list(any)
-  default = ["serviceAccount:seuserviceaccount@appspot.gserviceaccount.com"]
+  type        = list(any)
+  default     = ["serviceAccount:seuserviceaccount@appspot.gserviceaccount.com"]
 }
 
 
 
 variable "members_group" {
   description = "allowing google email group"
-  type    = list(any)
-  default = ["group:seugrupo@gmail.com"]
+  type        = list(any)
+  default     = ["group:seugrupo@gmail.com"]
 }
 
 
 
 variable "members_domain" {
   description = "allowing a specific domain"
-  type    = list(any)
-  default = ["domain:seudomain.com"]
+  type        = list(any)
+  default     = ["domain:seudomain.com"]
 }
 
 
 
+variable "pub_sub" {
+  description = "the pub/sub api"
+  type        = string
+  default     = "google.pubsub.topic.publish"
+}
 
+
+variable "pub_sub_topic" {
+  description = "the pub/sub topic name"
+  type        = string
+  default     = "projects/pj-ladriano-cloud/topics/topic-functions"
+
+}
 
 
 
