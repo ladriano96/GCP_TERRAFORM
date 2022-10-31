b5498a0fe7657f7c95e11154c1e3cb7925857732
ladriano96 <leonardoadriano93@gmail.com>
2022-10-31 13:07:33 -0300

    .

--- CLOUD-FUNCTIONS/cloud-functions.tf
+++ CLOUD-FUNCTIONS/cloud-functions.tf
@@ -14,13 +14,13 @@ resource "google_cloudfunctions_function" "cloudfunctions_g1_01" {
   source_archive_bucket = google_storage_bucket.storage_bucket.name
   source_archive_object = google_storage_bucket_object.storage_bucket_object.name
   /* (Se voce quiser usar vpc connector na function, basta descomentar a linha abaixo e criar o connector do arquivo vpc-connector.tf) */
-  vpc_connector = var.vpc_connector
+  #vpc_connector = var.vpc_connector
 
   labels = {
     "enviroment" = "dev"
   }
   /* descomentar o comentário abaixo "google_vpc_access_connector.vpc-connector-g1" para criar a function*/
-  depends_on = [google_storage_bucket_object.storage_bucket_object, google_vpc_access_connector.vpc-connector-g1]
+  depends_on = [google_storage_bucket_object.storage_bucket_object, /*google_vpc_access_connector.vpc-connector-g1*/]
 }
 
 
@@ -38,19 +38,22 @@ resource "google_cloudfunctions_function" "cloudfunctions_g1_02" {
   entry_point           = var.entry_point_name
   source_archive_bucket = google_storage_bucket.storage_bucket.name
   source_archive_object = google_storage_bucket_object.storage_bucket_object.name
-  vpc_connector         = var.vpc_connector
   available_memory_mb   = "512"
   max_instances         = "3"
   timeout               = "60"
+  /* (Se voce quiser usar vpc connector na function, basta descomentar a linha abaixo e criar o connector do arquivo vpc-connector.tf) */
+  #vpc_connector         = var.vpc_connector
+
   event_trigger {
     event_type = var.pub_sub
     resource   = var.pub_sub_topic
-    failure_policy {
+  failure_policy {
       retry = true
     }
   }
 
-  depends_on = [google_vpc_access_connector.vpc-connector-g1]
+  /* descomentar o comentário abaixo "google_vpc_access_connector.vpc-connector-g1" para criar a function*/
+  depends_on = [google_storage_bucket_object.storage_bucket_object, /*google_vpc_access_connector.vpc-connector-g1*/]
   labels = {
     "enviroment" = "dev"
   }
