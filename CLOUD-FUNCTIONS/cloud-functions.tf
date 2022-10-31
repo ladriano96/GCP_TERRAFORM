6bd2936a70af4ef20051fea5d3ae529357c192e8
ladriano96 <leonardoadriano93@gmail.com>
2022-10-31 12:02:09 -0300

    .

--- CLOUD-FUNCTIONS/cloud-functions.tf
+++ CLOUD-FUNCTIONS/cloud-functions.tf
@@ -1,4 +1,4 @@
-/* CLOUD FUNCTIONS 1ª GERACAO COM TRIGGER HTTP ATIVA SEM VPC CONNECTOR */
+/* CLOUD FUNCTIONS 1ª GERACAO COM TRIGGER HTTP SEM VPC CONNECTOR */
 resource "google_cloudfunctions_function" "cloudfunctions_g1_01" {
   name                  = "funct-${var.project_id}-${var.env}-01"
   runtime               = var.runtime_type
@@ -23,7 +23,7 @@ resource "google_cloudfunctions_function" "cloudfunctions_g1_01" {
 
 
 
-/* CLOUD FUNCTIONS 1ª GERACAO COM TRIGGER HTTP ATIVA E VPC CONNECTOR */
+/* CLOUD FUNCTIONS 1ª GERACAO COM TRIGGER HTTP E VPC CONNECTOR */
 resource "google_cloudfunctions_function" "cloudfunctions_g1_02" {
   name                  = "funct-${var.project_id}-${var.env}-02"
   runtime               = var.runtime_type
@@ -43,6 +43,10 @@ resource "google_cloudfunctions_function" "cloudfunctions_g1_02" {
     "enviroment" = "dev"
   }
 }
+
+
+
+
 
 
 /* CLOUD FUNCTIONS 1ª GERACAO COM EVENT TRIGGER PUB/SUB */
@@ -71,22 +75,4 @@ resource "google_cloudfunctions_function" "cloudfunctions_g1_03" {
   labels = {
     "enviroment" = "dev"
   }
-}
-
-
-
-
-
-/* VPC SERVLESS CONNECTOR */
-resource "google_vpc_access_connector" "vpc-connector-g1" {
-  provider      = google-beta.beta
-  name          = "cnt-${var.env}-01"
-  ip_cidr_range = var.subnet
-  network       = var.vpc
-  min_instances = "2"
-  max_instances = "3"
-  machine_type  = "f1-micro"
-
-  depends_on = [google_storage_bucket_object.storage_bucket_object]
-
 }
