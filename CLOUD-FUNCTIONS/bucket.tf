8aef4bde0d05a9eea38ed56d0efe844a8ab2524e
ladriano96 <leonardoadriano93@gmail.com>
2022-11-03 12:10:51 -0300

    .

--- CLOUD-FUNCTIONS/bucket.tf
+++ CLOUD-FUNCTIONS/bucket.tf
@@ -13,7 +13,7 @@ resource "google_storage_bucket" "storage_bucket" {
 resource "google_storage_bucket_object" "storage_bucket_object" {
   name       = "cloud-functions/code-functions"
   bucket     = google_storage_bucket.storage_bucket.name
-  source     = "seuarquivo.zip"
+  source     = "code-functions/index.zip"
   depends_on = [google_storage_bucket.storage_bucket]
 
 }
